aacf7490676e10765dcb3313998a0c858c62b700
Muza MK <1124muzammil@gmail.com>
2025-11-11 23:00:02 +0530

    Update Backend Mongo URI

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect('mongodb://mongo:27017/dockerlearn', {
+  .connect('process.env.MONGO_URI', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
